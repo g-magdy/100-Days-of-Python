94a205f23233942304f00a7c6dbdc9eafba775cc
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-02-02 10:01:08 +0200

    Finished hangman game

--- Week1/hangman.py
+++ Week1/hangman.py
@@ -1,86 +1,38 @@
+import os
 import random
+import hangman_art
+os.system('CLS')
 print("Welcome to Hangman")
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+print(hangman_art.hangmanArt)
 # those are the sample space from which the random word is chosen
-vocab = ["apple", "mango", "orange", "banana", "eggs", "beans", "aswan", "luxor", "oliver"]
-word = random.choice(vocab)
-letters_to_guess = len(word)
+#vocab = ["apple", "mango", "orange", "banana", "eggs", "beans", "aswan", "luxor", "oliver"]
+from hangman_words import word_list
+word = random.choice(word_list)
 guesslist = []
 for i in range(len(word)):
     guesslist.append('_')
 print(guesslist)
 #print(word)
 lives = 7
-while lives > 0 and letters_to_guess > 0:
+while lives > 0:
     l = input("Choose a letter\n").lower()
+    os.system('CLS') # really beautiful
+    if l in guesslist:
+        print("you guessed this correctly before")
+        continue
     right_choice = False
     for i in range(len(word)):
         if l == word[i].lower():
             right_choice = True
-            letters_to_guess -= 1
             guesslist[i] = l
     print(guesslist)
     if not right_choice:
+        print("You guessed a letter that is not in the word. You lose a life.")
         lives -= 1
-    if letters_to_guess == 0:
+    if '_' not in guesslist: # Important
         print("YOU WON 😀")
     elif lives == 0:
         print("Game over 😔")
-    if lives < 7 and letters_to_guess > 0:
-        print(stages[lives])
+        print(f"The correct word is {word}")
+    if lives < 7:
+        print(hangman_art.stages[lives])
