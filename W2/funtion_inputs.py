4bac081f5433d901f23165797ceb94b62339a094
George Magdy <georgetawfik09@gmail.com>
2023-02-02 23:02:23 +0200

    Start week 2

---
+++ W2/funtion_inputs.py
@@ -0,0 +1,7 @@
+# Design the interface to be readable, use abstraction as much as possible.
+def greet():
+    print("this is our way home")
+    print("Hello, this is a function")
+    print("Let's go home")
+
+greet()
