b3d712568368ffa676a94a05c653a5a2dac7b1a3
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-02-08 10:07:22 +0200

    Number guessing game done Python has no block scope

--- W2/local_tests.py
+++ W2/local_tests.py
@@ -1,19 +1,31 @@
-def func(a):
-    a.append(90)
-a = [1, 12] 
-func(a)
-print(a)
-'''def initialize():
-    print("Initialize")
+# def func(a):
+#     a.append(90)
+# a = [1, 12] 
+# func(a)
+# print(a)
 
-def hit():
-    print("Hit")
+# deck = [11, 3]
 
-def stand():
-    print("stand")
-'''
+# deck[deck.index(11)] = 1
+# print(deck) b
 
-deck = [11, 3]
+a = 10
 
-deck[deck.index(11)] = 1
-print(deck) 
+def f():
+    a = 20
+    print(a)
+
+f()
+
+# In python there is no block  scope : no scope for if or while or for 
+# the scope of the stuff inside s the same scope of the enclosing function definition or global
+a = 10
+
+if a > 5:
+    b = 123
+elif a > 15:
+    b = 90
+else:
+    b = 1
+    
+print(b)
