3c99005b23620cee829d5e95a4d99d34b12b830d
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-02-08 14:13:42 +0200

    Improve the design of the guessing game

--- W2/GuessTheNumber.py
+++ W2/GuessTheNumber.py
@@ -1,9 +1,8 @@
 import os, random
 from Art_of_w2 import Guess_the_number
-
 def Execute_main(number, lives):
+    '''play the main game - returns true if the user wins'''
     guess = 0 # initial value
-    won = False
     while guess != number and lives > 0:
         guess = int(input("What is your guess ? "))
         if guess < number:
@@ -15,20 +14,13 @@ def Execute_main(number, lives):
             print("Too high")
             print(f"Remaining lives = {lives}")
         else:
-            won = True
-    
-    if won:
-        print("You won the game 🎉")
-        return True
-    else:
-        print("You ran out of lives")
-        return False
-
-###################################
+            return True
+    return False # if he ran out of lives
+# START
 os.system("cls")
 print(Guess_the_number)
 print("Welcome to the number guessing game!\nI am thinking of a number between 1 and 100\nCan you guess it ?")
-# maybe a little feature cn be added to let the user choose the boundaries
+# maybe a little feature can be added to let the user choose the boundaries
 number = random.randint(1, 100)
 difficulty = input("Choose a dificulty (easy) / (hard): ")
 if difficulty == 'easy':
@@ -37,4 +29,8 @@ elif difficulty == 'hard':
     lives = 5
 else:
     lives = 7
-won_the_game = Execute_main(number, lives)
+if Execute_main(number, lives):
+    print("You won the game 🎉")
+else:
+    print("You ran out of lives")
+# END
