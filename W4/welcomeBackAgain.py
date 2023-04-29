28af9d6a9dbb4e09560b59bea4aea82ce54e709c
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-04-29 23:57:04 +0300

    almost nothing .. just trying to get back on track before going to sleep

---
+++ W4/welcomeBackAgain.py
@@ -0,0 +1,14 @@
+import os, random, time
+os.system("cls")
+# automatically starts i at 0 then increments till till it reaches 2
+for i in range(3):
+    print("Don't quit again")
+    # delay for 1 seconds
+    time.sleep(1)
+
+# clear all of the terminal screen 
+os.system("cls")
+
+for i in range(3):
+    print("Never quit")
+    time.sleep(1)
