3de29909d7b3ccdfb0cc08c8dd76ca39966e7a0c
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-05-06 09:29:39 +0300

    Make a better way to exit the game By clicking on "s", No warnings or errors appear in the terminal

--- Miscellaneous/recallSnake/snake_game.py
+++ Miscellaneous/recallSnake/snake_game.py
@@ -5,21 +5,30 @@ import time
 myScr = Screen()
 myScr.setup(width=600, height=600)
 myScr.bgcolor("black")
-myScr.title("This is the snake Game! you can exit by pressing e or clicking the screen")
+myScr.title("Snake Game! you can exit by pressing s")
 myScr.tracer(0)
 serpent = Snake()
 
+def close():
+    global game_is_on
+    game_is_on = False
+
+game_is_on = True
+
 myScr.listen()
-myScr.onkey(fun=myScr.bye, key="e") #PLEASE do not put parentheses here
+myScr.onkey(fun=close, key="s") #do not put parentheses after the function name
 myScr.onkey(fun=serpent.goUp, key="Up")
 myScr.onkey(fun=serpent.goDown, key="Down")
 myScr.onkey(fun= serpent.goLeft, key="Left")
 myScr.onkey(fun=serpent.goRight, key="Right")
 
-game_is_on = True
 while game_is_on:
-    serpent.move()
     myScr.update()
+    serpent.move()
     time.sleep(0.03)
 
-myScr.exitonclick()
+# this line is better than the exit on click,
+# because it does not print strange error messages on the terminal
+# when closing the window 
+# TODO: I can initiate a sequence to exit the game properly with a message
+myScr.bye()
