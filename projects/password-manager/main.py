385b139b928c9f73a83181fb36f1f59665fa42b2
George Magdy <113345460+GeorgeMagdyTawfik@users.noreply.github.com>
2023-08-11 23:20:01 +0300

    feat: users no can search for their saved data also, no duplicate websites are allowed

--- projects/password-manager/main.py
+++ projects/password-manager/main.py
@@ -1,12 +1,9 @@
 import tkinter, csv, random, pyperclip, json
 from tkinter import messagebox
 from string import digits, ascii_lowercase, ascii_uppercase
-#Potential features
-# 1. check for repeated entries of a website
-# 2. validate wbsite name
-# #
 # ---------------------------- CONSTANTS ------------------------------- #
-DEFAULT_EMAIL = "georgetwafik09@gmail.com"
+# TODO: change this email :)
+DEFAULT_EMAIL = "jerry@gmail.com"
 SYMBOLS =['!', '#', '$', '%', '&', '*', '(', ')', '+']
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pwd():
@@ -29,7 +26,7 @@ def generate_pwd():
             
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def register():
-    website = website_in.get()
+    website = website_in.get().lower()
     email = email_in.get()
     password = pwd_in.get()
     if "" in [website, email, password]:
@@ -37,7 +34,17 @@ def register():
                              message="Please fill out all fields")
         return
     
-    confirmation = messagebox.askokcancel(title=f"{website}",
+    # check if the website name already exists!
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            if website in data:
+                messagebox.showwarning(title="Could not add website", message="Website already exists!")
+                return
+    except FileNotFoundError:
+        messagebox.showerror("data.json was not found")
+    
+    confirmation = messagebox.askokcancel(title=f"{website.capitalize()}",
                                    message=f"Email: {email}\nPassword: {password}\nSave this ?")
     if confirmation:
         new_data = {
@@ -62,21 +69,46 @@ def register():
             pyperclip.copy(password)
             messagebox.showinfo(title="Done!", 
                                 message="Your data was saved in data.json\n"
-                                "Your password was compied to the clipboard too!")
+                                "Your password was copied to the clipboard too!")
 
+# ---------------------------- FIND PREVIOUSLY SAVED DATA ------------------------------- #
+def find_website():
+    website = website_in.get().lower()
+    if website != "":
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                answer = messagebox.askyesno(
+                    title=website.capitalize(),
+                    message=f"email : {data[website]['email']}\n"
+                    f"password : {data[website]['password']}\n"
+                    "Do you want me to copy the password to the clipboard ?"
+                )
+                if answer:
+                    pyperclip.copy(data[website]["password"])
+                    messagebox.showinfo(title="success", message="password was copied!")
+        except FileNotFoundError:
+            messagebox.showerror(title="Could not search", message="data.json was not found")
+        except KeyError as k:
+            messagebox.showerror(title="Key does not exist", message=f"{k} does not exist in data.json")
+    else:
+        messagebox.showwarning(title="Could not search", message="Website name cannot be empty")
+    
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+# creating widgets
 canvas = tkinter.Canvas(height=200, width=200)
 img = tkinter.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
 website_label = tkinter.Label(text="Website: ")
-email_label = tkinter.Label(text="Email/Username: ")
-pwd_label = tkinter.Label(text="Password: ")
+search_button = tkinter.Button(text="Search", width=10, command=find_website)
 website_in = tkinter.Entry(width=35)
 website_in.focus()
+email_label = tkinter.Label(text="Email/Username: ")
+pwd_label = tkinter.Label(text="Password: ")
 email_in = tkinter.Entry(width=35)
 email_in.insert(0, DEFAULT_EMAIL)
 pwd_in = tkinter.Entry(width=35)
@@ -88,6 +120,7 @@ canvas.grid(row=0, column=1)
 
 website_label.grid(row=1, column=0)
 website_in.grid(row=1, column=1, columnspan=2)
+search_button.grid(row=1, column=3)
 
 email_label.grid(row=2, column=0)
 email_in.grid(row=2, column=1, columnspan=2)
