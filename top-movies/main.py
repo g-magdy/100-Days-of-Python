635f666b6ef226d38a41fcb08c5a575cae9d7a4e
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-07-16 09:26:35 +0300

    reab movies from sqlite db

--- top-movies/main.py
+++ top-movies/main.py
@@ -26,14 +26,33 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+  pass
 
+db = SQLAlchemy(model_class=Base)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db.init_app(app)
 # CREATE TABLE
 
+#! Mapped[int] is important
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    year : Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String, nullable=False)
+    img_url: Mapped[str] = mapped_column(String, nullable=False)
+
+def getAllMovies():
+    return db.session.execute(db.select(Movie)).scalars().all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", movies=getAllMovies())
 
 
 if __name__ == '__main__':
