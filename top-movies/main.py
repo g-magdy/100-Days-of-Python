69b447c6d8add9f9a89d8a6c49d3ba0977dddc0b
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-07-16 12:12:23 +0300

    implement server-side caching

--- top-movies/main.py
+++ top-movies/main.py
@@ -1,11 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
-# from flask_wtf import FlaskForm
-# from flask_wtf.form import _Auto
-# from wtforms import StringField, EmailField, PasswordField, SubmitField
-# from wtforms.validators import DataRequired, Length, Email
-# import email_validator
-# from flask_bootstrap import Bootstrap5
-
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -14,6 +7,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired
 import requests
+from dotenv import dotenv_values
+from flask_caching import Cache
+
 
 class EditForm(FlaskForm):
     rating = FloatField(label="Your rating out of 10 e.g. 7.5", validators=[DataRequired()])
@@ -21,9 +17,24 @@ class EditForm(FlaskForm):
     submit = SubmitField("Done")
 
 
+class AddForm(FlaskForm):
+    movieName = StringField(label="Search for Movies by Name", validators=[DataRequired()])
+    submit = SubmitField("Search")
+
+MOVIES_API_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": dotenv_values().get("AUTH_TOKEN")
+}
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+# Configure Flask-Caching
+app.config['CACHE_TYPE'] = 'simple'  # Simple in-memory cache
+cache = Cache(app)
 
 # CREATE DB
 class Base(DeclarativeBase):
@@ -68,8 +79,45 @@ def editRating():
         
     return render_template("edit.html", form = EditForm(), movie=req_movie)
     
-        
+@app.route("/delete")
+def deleteMovie():
+    target = request.args.get("id")
+    req_movie = db.get_or_404(Movie, target)
+    db.session.delete(req_movie)
+    db.session.commit()
+    return redirect(url_for("home"))
 
+
+@app.route("/add", methods=["GET", "POST"])
+def addMovie():
+    addForm : FlaskForm = AddForm()
+    
+    if addForm.validate_on_submit():
+        params = {
+            "query": addForm.movieName.data
+        }
+        moviesResponse = requests.get(url=MOVIES_API_URL, headers=headers, params=params)
+        moviesResponse = moviesResponse.json()
+        # session["searchData"] = moviesResponse
+        
+        cache.set('searchData', moviesResponse["results"], timeout=5*60)  # Store in cache for 5 minutes
+
+        # print(session.get("searchData", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+        return redirect(url_for('pick'))
+    
+    return render_template("add.html", form=addForm)
+
+@app.route("/search")
+def pick():
+    # print(session.get("searchData", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+    # searchData = session.get('searchData', [])  # Retrieve the data from the session
+    searchData = cache.get('searchData')  # Retrieve the data from the cache
+    return render_template("select.html", movies=searchData)
+
+@app.route("/get-movie")
+def getMovieDetails():
+    params = request.args.get("id")
+    return f"you requested moive id = {params}"
 
 if __name__ == '__main__':
     app.run(debug=True)
